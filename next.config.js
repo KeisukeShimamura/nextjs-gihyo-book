55fca6bc3b00db01e5dd414210d6bcc33b76d73a
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2022-08-02 21:30:22 +0900

    ReactTestingLibraryのセットアップ

--- next.config.js
+++ next.config.js
@@ -2,10 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    // styledComponentsの有効化
-    styledComponents: true,
-  }
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsの有効化
+      styledComponents: true,
+    }
+    
+    if (process.env.NODE_ENV == 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdate-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
